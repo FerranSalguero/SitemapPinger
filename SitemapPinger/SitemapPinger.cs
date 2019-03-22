635b2df334d949fb151c1fa56b99e8f12d41b0f0
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2019-03-22 20:51:24 +0100

    Update SitemapPinger.cs

--- SitemapPinger/SitemapPinger.cs
+++ SitemapPinger/SitemapPinger.cs
@@ -22,7 +22,7 @@ namespace WebsitePinger
         public SitemapPinger()
         {
             Urls = new string[] {
-                "https://www.havetoget.com/sitemap/index",
+                "https://whereshouldibuyineurope.apphb.com/sitemap/index",
             };
 
             
